9db79e2c5c981bcb22d530c376be66e837d2f774
Maicon Cerutti <maicon@litelims.com>
2019-01-16 17:43:36 -0200

    Instalação do Laravel Collective, e utilizado o comando de criação de Usuários nativo do Laravel (Auth)

--- config/app.php
+++ config/app.php
@@ -161,11 +161,14 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
 
+        // Para instalação do Laravel Collective
+        Collective\Html\HtmlServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -223,6 +226,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Para instalação do Laravel Collective
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Maicon Cerutti
                 </div>
 
                 <div class="links">
