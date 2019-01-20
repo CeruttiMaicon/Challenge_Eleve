1ad3f444a846a4288bebf57e7af5ad99d1d1554d
Maicon Cerutti <maicon@litelims.com>
2019-01-20 11:34:23 -0200

    Ajustado arquivos CSS e JS, agora estão minificados, cmo pede no desafio.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,16 +10,16 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.min.js') }}" defer></script>
    
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css"> 
-    <link href="{{ asset('css/template.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/template.min.css') }}" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.min.css') }}" rel="stylesheet">
 
 
     <!-- Include this after the sweet alert js file -->
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js/app.min.js')
    .js('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css/app.min.css')
+   .styles(['resources/css/template.css'], 'public/css/template.min.css');
